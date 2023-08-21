867edae52adccd35e89ec4876e63d617f3082fb3
Mpho_Ndlovu <126378512+MphoPetunia@users.noreply.github.com>
2023-08-21 14:10:46 +0200

    Updated script.js

--- src/script.js
+++ src/script.js
@@ -38,7 +38,7 @@ function displayWeatherCondition(response) {
 }
 
 function searchCity(city) {
-  let apiKey = "b9ba0314a93083136d968577c718e31d";
+  let apiKey = "1886ea3c1f4e905fa3d33dfa300b3948";
   let apiUrl = `https://api.openweathermap.org/data/2.5/weather?q=${city}&appid=${apiKey}&units=metric`;
   axios.get(apiUrl).then(displayWeatherCondition);
 }
@@ -50,7 +50,7 @@ function handleSubmit(event) {
 }
 
 function searchLocation(position) {
-  let apiKey = "b9ba0314a93083136d968577c718e31d";
+  let apiKey = "1886ea3c1f4e905fa3d33dfa300b3948";
   let apiUrl = `https://api.openweathermap.org/data/2.5/weather?lat=${position.coords.latitude}&lon=${position.coords.longitude}&appid=${apiKey}&units=metric`;
 
   axios.get(apiUrl).then(displayWeatherCondition);
